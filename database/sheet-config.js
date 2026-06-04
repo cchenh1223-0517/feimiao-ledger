4ef6efa6d368184b76f789a16a7e4d105caca7de
cchenh1223-0517 <cchenh1223@gmail.com>
2026-06-04 23:36:38 +0800

    Update sheet-config.js

--- database/sheet-config.js
+++ database/sheet-config.js
@@ -1,1 +1,1 @@
-window.FEI_MIAO_SHEET_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw7zwItvfWd4M3agZsr6YSgOHux1n5ms2OMpXx18JTzAiK-srpBv1lW2Vpq8aGymrPH/exec";
+window.FEI_MIAO_SHEET_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwxuTH92Bca0lcJnILNQxEIGp2A_TDtogpwGyc81mxZjEqjBac829EuOUFEYl547Ijs/exec";
